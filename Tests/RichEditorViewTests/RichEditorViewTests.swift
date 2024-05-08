2be8cf7c1812e1a5ebd43eb744b8e16bea63eb2f
zeroskylian <zeroskylian@gmail.com>
2024-05-08 11:02:47 +0800

    fix build error

--- Tests/RichEditorViewTests/RichEditorViewTests.swift
+++ Tests/RichEditorViewTests/RichEditorViewTests.swift
@@ -2,11 +2,12 @@ import XCTest
 @testable import RichEditorView
 
 final class RichEditorViewTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(RichEditorView().text, "Hello, World!")
+        XCTAssertEqual(RichEditorView().html, "Hello, World!")
     }
 
     static var allTests = [
